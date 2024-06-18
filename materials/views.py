82bfa3de8d3b519eb5f0d400c7502e52277bd577
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-18 17:30:01 +0500

    Добавить измененный файл materials/views.py

--- materials/views.py
+++ materials/views.py
@@ -5,29 +5,47 @@ from materials.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели Course
+    """
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
+    """
+    API для создания нового урока
+    """
     serializer_class = LessonSerializer
 
 
 class LessonListAPIView(generics.ListAPIView):
+    """
+    API для получения списка уроков
+    """
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    API для получения урока
+    """
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
+    """
+    API для изменения урока
+    """
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
+    """
+    API для удаления урока
+    """
     queryset = Lesson.objects.all()
 
