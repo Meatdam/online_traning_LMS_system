2fd1403c345cdd3e212d9fdba36050a1d0a5fe20
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-19 19:36:20 +0500

    Добавить измененный файл base/settings.py
    добавить настройку django_filters

--- base/settings.py
+++ base/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_filters',
     'rest_framework',
+
     'users',
     'materials',
 ]
@@ -133,3 +135,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+}
+
