4659d824214358a2f86ea52c8d938fe123cf8969
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-24 20:50:24 +0500

    добавить измененный файл users/views.py
    Добавить CRUD User

--- users/views.py
+++ users/views.py
@@ -1,18 +1,60 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework import generics
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
 
 from users.models import User, Payments
+from users.permissions import IsUserOrStaff
 from users.serializers import UserSerializer, PaymentsSerializer
 
 
-class UserViewSet(ModelViewSet):
+class UserListApiView(generics.ListAPIView):
     """
-    ViewSet для модели User
+    API для получения списка пользователей
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreateApiView(generics.CreateAPIView):
+    """
+    API для регистрации пользователя
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserRetrieveApiView(generics.RetrieveAPIView):
+    """
+    API для получения пользователя
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """
+    API для изменения пользователя
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsUserOrStaff]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    """
+    API для удаления пользователя
     """
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    permission_classes = [IsUserOrStaff]
 
 
 class PaymentsListAPIView(generics.ListAPIView):
